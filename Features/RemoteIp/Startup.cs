c10dc2b2b6db6215cce70cf4207d694423fabb06
Luis Guerrero <laguer71@hotmail.com>
2019-09-16 22:22:16 -0500

    Add project files.

---
+++ Features/RemoteIp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+[assembly: OrchardCore.Modules.Manifest.Feature(
+    Id = "RemoteIp",
+    Name = "RemoteIp"
+)]
+
+namespace MultiTenantApp.Features.RemoteIp
+{
+    [Feature("RemoteIp")]
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IMessageProvider, RemoteIpMessageProvider>();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell;
+using Microsoft.Extensions.Hosting;
+
+namespace MultiTenantApp
+{
+   public class Startup
+   {
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services
+             .AddOrchardCore()
+             .WithTenants()
+             ;
+      }
+
+      public void Configure(IApplicationBuilder app, IHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+         }
+
+         app.UseOrchardCore(a => a.Run(async context =>
+         {
+            // ShellSettings provide the tenant's configuration.
+            var shellSettings = context.RequestServices.GetRequiredService<ShellSettings>();
+
+            var tenant = shellSettings.Name;
+
+            // Read the tenant-specific custom setting.
+            var customSetting = shellSettings["CustomSetting"];
+
+            // Resolve all registered IMessageProvider services.
+            var messageProviders = context.RequestServices.GetServices<IMessageProvider>();
+
+            // Invoke all IMessageProviders.
+            var messages = (await Task.WhenAll(messageProviders.Select(async x => await x.GetMessageAsync()))).ToList();
+
+            // Add the custom setting as a message. Alternatively, could have implemented another IMessageProvider that reads the
+            messages.Insert(0, tenant);
+            messages.Insert(1, customSetting);
+
+            // Concatenate all messages.
+            var output = string.Join("\r\n", messages);
+
+            // Write the output string to the response stream.
+            await context.Response.WriteAsync(output);
+         }));
+      }
+   }
+}
